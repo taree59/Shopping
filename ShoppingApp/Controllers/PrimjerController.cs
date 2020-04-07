c801ea28a4cd5884edaa233a7a14e55136c77295
mirsada <tarik905.suljic@hotmail.com>
2020-04-07 21:52:50 +0200

    Add project files.

---
+++ ShoppingApp/Controllers/PrimjerController.cs
@@ -0,0 +1,21 @@
+﻿using ShoppingApp.Model;
+using System.Linq;
+using System.Web.Mvc;
+
+namespace ShoppingApp.Controllers
+{
+    public class PrimjerController : Controller
+    {
+        private ShoppingDbEntities db = new ShoppingDbEntities();
+
+        public ActionResult Index()
+        {
+            return View(db.Shoppings.ToList());
+        }
+
+        public ActionResult About()
+        {
+            return View(db.Shoppings.ToList());
+        }
+    }
+}
---
+++ ShoppingApp/Controllers/ShoppingsController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using ShoppingApp.Model;
+
+namespace ShoppingApp.Controllers
+{
+    public class ShoppingsController : Controller
+    {
+        private ShoppingDbEntities db = new ShoppingDbEntities();
+
+        // GET: Shoppings
+        public ActionResult Index()
+        {
+            return View(db.Shoppings.ToList());
+        }
+
+        // GET: Shoppings/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Shopping shopping = db.Shoppings.Find(id);
+            if (shopping == null)
+            {
+                return HttpNotFound();
+            }
+            return View(shopping);
+        }
+
+        // GET: Shoppings/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Shoppings/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "ShoppingID,ShoppingName,Address,Phone")] Shopping shopping)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Shoppings.Add(shopping);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(shopping);
+        }
+
+        // GET: Shoppings/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Shopping shopping = db.Shoppings.Find(id);
+            if (shopping == null)
+            {
+                return HttpNotFound();
+            }
+            return View(shopping);
+        }
+
+        // POST: Shoppings/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "ShoppingID,ShoppingName,Address,Phone")] Shopping shopping)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(shopping).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(shopping);
+        }
+
+        // GET: Shoppings/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Shopping shopping = db.Shoppings.Find(id);
+            if (shopping == null)
+            {
+                return HttpNotFound();
+            }
+            return View(shopping);
+        }
+
+        // POST: Shoppings/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Shopping shopping = db.Shoppings.Find(id);
+            db.Shoppings.Remove(shopping);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
